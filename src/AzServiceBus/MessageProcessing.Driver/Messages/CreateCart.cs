454e99181ac3371ba6745d459d2b711ab96d1f5b
JaiGanesh <jaiganesh.sundaravel@outlook.com>
2017-05-26 20:04:18 +0100

    Added initial setup.

---
+++ src/AzServiceBus/MessageProcessing.Driver/Messages/CreateCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageProcessing.Driver.Messages
+{
+    public class CreateCart
+    {
+        public string CustomerId { get; set; }
+        public string CartId { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
--- src/AzServiceBus/MessageProcessing.Driver/Program.cs
+++ src/AzServiceBus/MessageProcessing.Driver/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,35 @@ namespace MessageProcessing.Driver
     {
         static void Main(string[] args)
         {
+            var config = new MessageSenderConfig();
+            MainAsync(args).GetAwaiter().GetResult();
         }
+
+        static async Task MainAsync(string[] args)
+        {
+            string sharedAccessKey = "";
+            string keyName = "";
+            string queuePath = "CreateCart";
+            string namespaceAddress = "";
+            var senderFactory = MessagingFactory.Create(
+                namespaceAddress,
+                new MessagingFactorySettings
+                {
+                    OperationTimeout = TimeSpan.FromSeconds(20),
+                    TransportType = (TransportType)Enum.Parse(typeof(TransportType),"Amqp",true),
+                    TokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(keyName, sharedAccessKey)
+                });
+            var sender = await senderFactory.CreateMessageSenderAsync(queuePath);
+        }
+    }
+
+    public class MessageSenderConfig
+    {
+        public string NamespaceAddress { get; set; }
+        public string KeyName { get; set; }
+        public string SharedAccessKey { get; set; }
+        public string Path { get; set; }
+        public string TransportType { get; set; } = "Amqp";
+        public TimeSpan OperationTimeout { get; set; } = TimeSpan.FromSeconds(20);
     }
 }
